32aee99bb368170a8b7a63b24f386b6ce7f19c1e
zanehan <zhan5@wgu.edu>
2025-10-16 01:58:00 +0800

    Finish pipeline setup; local env + steps wired

--- main.py
+++ main.py
@@ -1,97 +1,151 @@
-import json
-
-import mlflow
-import tempfile
 import os
-import wandb
-import hydra
-from omegaconf import DictConfig
+import json
+import tempfile
+from pathlib import Path
 
-_steps = [
+import hydra
+import mlflow
+from omegaconf import DictConfig
+from hydra.utils import get_original_cwd
+
+_ALL_STEPS = [
     "download",
     "basic_cleaning",
     "data_check",
     "data_split",
     "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-#    "test_regression_model"
+    # "test_regression_model",
 ]
 
 
-# This automatically reads in the configuration
-@hydra.main(config_name='config')
-def go(config: DictConfig):
+def _component_uri(repo: str, subdir: str) -> str:
+    repo = repo.strip().rstrip("/")
+    if "#components" in repo:
+        return f"{repo}/{subdir}"
+    return f"{repo}#components/{subdir}"
 
-    # Setup the wandb experiment. All runs will be grouped under this name
+
+def _full_artifact_ref(project: str, name_with_alias: str, entity: str = "") -> str:
+    base = f"{(entity or os.environ.get('WANDB_ENTITY', '')).strip('/')}/{project.strip('/')}".replace("//", "/")
+    return f"{base}/{name_with_alias}".replace("//", "/").strip("/")
+
+
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def go(config: DictConfig) -> None:
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
+    if "wandb_entity" in config["main"]:
+        os.environ["WANDB_ENTITY"] = str(config["main"]["wandb_entity"])
 
-    # Steps to execute
-    steps_par = config['main']['steps']
-    active_steps = steps_par.split(",") if steps_par != "all" else _steps
+    steps = (
+        _ALL_STEPS
+        if str(config["main"]["steps"]).strip().lower() == "all"
+        else [s.strip() for s in str(config["main"]["steps"]).split(",") if s.strip()]
+    )
 
-    # Move to a temporary directory
-    with tempfile.TemporaryDirectory() as tmp_dir:
+    components_repo = str(config["main"]["components_repository"]).strip()
+    project_name = config["main"]["project_name"]
+    wandb_entity = os.environ.get("WANDB_ENTITY", "")
 
-        if "download" in active_steps:
-            # Download file and load in W&B
+    orig_cwd = Path(get_original_cwd()).resolve()
+    basic_cleaning_uri = str((orig_cwd / "src" / "basic_cleaning").resolve())
+    data_check_uri = str((orig_cwd / "src" / "data_check").resolve())
+    train_rf_uri = str((orig_cwd / "src" / "train_random_forest").resolve())
+
+    tmp_dir = tempfile.TemporaryDirectory()
+    try:
+        os.chdir(tmp_dir.name)
+
+        if "download" in steps:
+            print(">> Step: download")
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/get_data",
-                "main",
-                version='main',
-                env_manager="conda",
+                uri=_component_uri(components_repo, "get_data"),
+                entry_point="main",
+                env_manager="local",
                 parameters={
                     "sample": config["etl"]["sample"],
-                    "artifact_name": "sample.csv",
-                    "artifact_type": "raw_data",
-                    "artifact_description": "Raw file as downloaded"
+                    "artifact_name": config.get("download", {}).get("artifact_name", "sample.csv"),
+                    "artifact_type": config.get("download", {}).get("artifact_type", "raw_data"),
+                    "artifact_description": config.get("download", {}).get(
+                        "artifact_description", "Raw_file_as_downloaded"
+                    ),
                 },
             )
 
-        if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+        if "basic_cleaning" in steps:
+            print(">> Step: basic_cleaning")
+            _ = mlflow.run(
+                uri=basic_cleaning_uri,
+                entry_point="main",
+                env_manager="local",
+                parameters={
+                    "input_artifact": f"{config.get('download', {}).get('artifact_name', 'sample.csv')}:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_data",
+                    "output_description": "Data_after_basic_cleaning",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
 
-        if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+        if "data_check" in steps:
+            print(">> Step: data_check")
+            ref = _full_artifact_ref(project_name, "clean_sample.csv:latest", wandb_entity)
+            _ = mlflow.run(
+                uri=data_check_uri,
+                entry_point="main",
+                env_manager="local",
+                parameters={
+                    "csv": ref,
+                    "ref": ref,
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
 
-        if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+        if "data_split" in steps:
+            print(">> Step: data_split")
+            input_ref = _full_artifact_ref(project_name, "clean_sample.csv:latest", wandb_entity)
+            _ = mlflow.run(
+                uri=_component_uri(components_repo, "train_val_test_split"),
+                entry_point="main",
+                env_manager="local",
+                parameters={
+                    "input_artifact": input_ref,
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                },
+            )
 
-        if "train_random_forest" in active_steps:
+        if "train_random_forest" in steps:
+            print(">> Step: train_random_forest")
+            rf_config_path = Path(tmp_dir.name) / "rf_config.json"
+            with open(rf_config_path, "w", encoding="utf-8") as fp:
+                json.dump(dict(config["modeling"]["random_forest"].items()), fp)
 
-            # NOTE: we need to serialize the random forest configuration into JSON
-            rf_config = os.path.abspath("rf_config.json")
-            with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
+            train_ref = _full_artifact_ref(project_name, "train.csv:latest", wandb_entity)
+            val_ref = _full_artifact_ref(project_name, "val.csv:latest", wandb_entity)
 
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
+            _ = mlflow.run(
+                uri=train_rf_uri,
+                entry_point="main",
+                env_manager="local",
+                parameters={
+                    "train_data": train_ref,
+                    "val_data": val_ref,
+                    "rf_config": str(rf_config_path),
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export",
+                },
+            )
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
-
-        if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+    finally:
+        os.chdir(orig_cwd)
+        tmp_dir.cleanup()
 
 
 if __name__ == "__main__":
     go()
+
